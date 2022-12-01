ab3596a8071e61178c24ec994abfe597ddf00ec9
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-12-01 17:43:23 +0900

    test: 랜덤 숫자 생성 테스트 코드 작성

--- src/main/kotlin/baseball/domain/BaseballService.kt
+++ src/main/kotlin/baseball/domain/BaseballService.kt
@@ -1,4 +1,10 @@
 package baseball.domain
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class BaseballService {
+
+    fun makeNumbers(): List<Int> {
+        return listOf()
+    }
 }
---
+++ src/test/kotlin/baseball/domain/BaseballServiceTest.kt
@@ -0,0 +1,26 @@
+package baseball.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class BaseballServiceTest {
+    private val baseballService = BaseballService()
+
+    @Test
+    fun `숫자 생성 함수 길이 테스트`() {
+        assertThat(baseballService.makeNumbers()).hasSize(3)
+    }
+
+    @Test
+    fun `숫자 생성 함수 원소 테스트`() {
+        val testNumbers = baseballService.makeNumbers()
+        for (testNumber in testNumbers) {
+            assertThat(testNumber).isBetween(START, END).also { println(it) }
+        }
+    }
+
+    companion object {
+        private const val START = 1
+        private const val END = 9
+    }
+}
