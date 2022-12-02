373df9798e4c810419471e30be3ba1253bfc9cbf
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-12-02 19:40:42 +0900

    feat: 게임 숫자 및 게임 상태 입력 함수 기능 구현

--- src/main/kotlin/baseball/ui/input/InputView.kt
+++ src/main/kotlin/baseball/ui/input/InputView.kt
@@ -1,4 +1,26 @@
 package baseball.ui.input
 
 class InputView {
+
+    private val validateInput = ValidateInput()
+
+    fun readGameNumbers(numbers: String): List<Int> {
+        return try {
+            validateInput.validateGameNumbers(numbers)
+            numbers.map { it.digitToInt() }
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            readGameNumbers(numbers)
+        }
+    }
+
+    fun readStatusNumber(number: String): Int {
+        return try {
+            validateInput.validateStatusNumber(number)
+            number.toInt()
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            readStatusNumber(number)
+        }
+    }
 }
