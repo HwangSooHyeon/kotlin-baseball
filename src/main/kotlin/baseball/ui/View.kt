0eef5dafe79982fa4b7b9f929d38c2b7e2c8ed49
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-12-05 13:22:35 +0900

    feat: 매칭 후 볼, 스트라이크 갯수 출력하는 기능 추가

--- src/main/kotlin/baseball/ui/View.kt
+++ src/main/kotlin/baseball/ui/View.kt
@@ -1,4 +1,17 @@
 package baseball.ui
 
+import baseball.ui.input.InputView
+import baseball.ui.output.OutputView
+
 class View {
+
+    private val inputView = InputView()
+    private val outputView = OutputView()
+    fun startView() = outputView.printStart()
+
+    fun inputGameNumberView(): List<Int> = inputView.readGameNumbers(inputView.getNumbers())
+
+    fun countView(count: String) = outputView.printCount(count)
+
+
 }
--- src/main/kotlin/baseball/ui/output/OutputView.kt
+++ src/main/kotlin/baseball/ui/output/OutputView.kt
@@ -1,4 +1,15 @@
 package baseball.ui.output
 
+import baseball.enums.GameMessages
+
 class OutputView {
+    fun printStart() = println(GameMessages.START.toString())
+
+    fun printInputGameNumbers() = println(GameMessages.INPUT_GAME_NUMBERS.toString())
+
+    fun printEnd() = println(GameMessages.END.toString())
+
+    fun printInputStatusNumber() = println(GameMessages.INPUT_STATUS_NUMBER.toString())
+
+    fun printCount(count: String) = println(count)
 }
