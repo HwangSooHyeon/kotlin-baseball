e17b82840721fb32f1f5d340771f3e43a47ad444
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-12-05 13:53:26 +0900

    feat: controller 및 main 구현

--- src/main/kotlin/baseball/controller/GameController.kt
+++ src/main/kotlin/baseball/controller/GameController.kt
@@ -1,4 +1,45 @@
 package baseball.controller
 
+import baseball.domain.BaseballService
+import baseball.model.Baseball
+import baseball.ui.View
+
 class GameController {
+
+    private val view = View()
+    private val baseballService = BaseballService()
+
+    fun play() {
+        do {
+            val createdNumbers = start()
+        } while (inGame(createdNumbers))
+    }
+
+    private fun start(): Baseball {
+        view.startView()
+        return Baseball(baseballService.makeNumbers())
+    }
+
+    private fun inGame(createdNumbers: Baseball): Boolean {
+        do {
+            val inputNumbers = Baseball(view.inputGameNumbersView())
+            val count = createdNumbers.compare(inputNumbers)
+            view.countView(count)
+        } while (end(count))
+        return restart()
+    }
+
+    private fun end(count: String): Boolean {
+        if (count == "3스트라이크")
+            return false
+        return true
+    }
+
+    private fun restart(): Boolean {
+        val statusNumber = view.inputStatusNumberView()
+        if (statusNumber == 1) {
+            return true
+        }
+        return false
+    }
 }
